81f9c5bdb60a7e29aca5c50c3e87112fbfc089e9
MARCUS VINICIUS <24287467-MARCUSVINICI297@users.noreply.replit.com>
2024-08-26 22:54:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let idade = 17;
+const anoNascimento = 2007;
+let anoAtual = 2024;
+const nome Completo = "Marcus Vinícius Correia";
+
+console.log( "Meu nome é " + nomeCompleto + " nascido no ano de " + ano Nascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
+
+anoAtual = anoAtual + 1;
+
+console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
+
+//Agência de viagens Argentina Malta Espanha Chile Indonésia
+const listaDeDestinos = new Array(
+  "Argentina",
+  "Malta",
+  "Espanha",
+  "Chile",
+  "Indonésia"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[4]);
