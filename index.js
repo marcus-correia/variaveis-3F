b6284830e14181611246a0059e74e016a61ea046
MARCUS VINICIUS <24287467-MARCUSVINICI297@users.noreply.replit.com>
2024-09-30 23:03:21 +0000

    30/09/2024

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 let idade = 17;
 const anoNascimento = 2007;
 let anoAtual = 2024;
-const nomeCompleto = "Marcus Vinícius Correia";
+const nomeCompleto = "Marcus Vinicius Correia";
 
-console.log( "Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
+idade = idade +1;
 
 console.log(anoAtual);
-
-idade = idade + 1;
 console.log(idade);
 
-//Agência de viagens Argentina Malta Espanha Chile Indonésia
+//agência de viagens Dubai, New York, Maldivas, Madagascar, Chile
+
 const listaDeDestinos = new Array(
-  "Argentina",
-  "Malta",
-  "Espanha",
-  "Chile",
-  "Indonésia"
-);
+  " Dubai",
+  " New York",
+  " Maldivas",
+  " Madagascar",
+  " Chile"
+ );
 
 console.log(listaDeDestinos);
-console.log(listaDeDestinos[4]);
-listaDeDestinos.push("Coronel Vivida")
-listaDeDestinos.push("Estocolmo")
-listaDeDestinos.push("Liubliana")
-listaDeDestinos.push("Roma")
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel Vivida");
 console.log(listaDeDestinos);
+listaDeDestinos.push("Austrália");
+listaDeDestinos.push("Alemanha");
+listaDeDestinos.push("Amazonas");
+console.log(listaDeDestinos);
+
+const loginCerto = "Marcus";
+const senhaCerta = 123;
+var login = entrada("Qual é o seu login? ");
+var senha = entrada("Qual é a sua senha: ");
+while (login != loginCerto || senha != senhaCerta){
+  console.log ( "Login ou senha incorretos");
+  login = entrada("Qual é o seu login? ")
+  senha = entrada("Qual é a sua senha: ")
+  
+}
+
+var nomeComprador = entrada("digite seu nome: ");
+var idadeComprador = entrada("Qual a sua idade ?");
+
+
+if(idadeComprador >=18) {
+  console.log("Olá senhor(a) "+ nomeComprador);
+  console.log("Comprador maior de idade");
+  console.log("Destinos disponíveis: " + listaDeDestinos);
+  console.log("escolha seu destinos");
+  var contador = 0;
+
+  while(contador <9){
+    console.log(listaDeDestinos[contador]);
+    contador ++;
+  }
+  var destino = entrada("Digite o número que corresponde ao destino selecionado (0-8)");
+  console.log(`destino selecionado foi: ${listaDeDestinos[destino]}`);
+} else {
+  console.log("Olá senhor(a)" + nomeComprador);
+  console.log("Infelizmente esta indisponivel passagem para menores de 18 anos");
+  console.log("Você e menor de idade");
+}
